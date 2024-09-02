fad13cf5ba7358e6646089e5a0f56191fa9e217e
c-a-r-l-x <dev.carlx@gmail.com>
2024-09-02 11:02:22 -0400

    Initial Commit

---
+++ TIL/TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by carla on 02-09-24.
+//
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
